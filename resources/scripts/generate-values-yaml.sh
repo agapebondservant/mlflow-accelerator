6a5566810035dc008fdc9e23afd9818cd547b91e
Tola Awofolu <oawofolu@pivotal.io>
2023-12-27 08:53:23 -0500

    Added TSL enabled minio endpoint

--- resources/scripts/generate-values-yaml.sh
+++ resources/scripts/generate-values-yaml.sh
@@ -7,8 +7,8 @@ export MLFLOW_DB_USER=pgadmin
 export MLFLOW_DB_PASSWORD=$(kubectl get secret pg-mlflow-instance-db-secret -n ${POSTGRES_CLUSTER_NAMESPACE} -o jsonpath="{.data.password}" | base64 --decode)
 export MLFLOW_DB_URI=postgresql://${MLFLOW_DB_USER}:${MLFLOW_DB_PASSWORD}@${MLFLOW_DB_HOST_LOCAL}:5432/${MLFLOW_DB_NAME}?sslmode=require
 export MLFLOW_S3_ENDPOINT_URL=https://${MLFLOW_S3_ENDPOINT_FQDN}
-export AWS_ACCESS_KEY_ID=$(kubectl get secret minio -o jsonpath="{.data.accesskey}" -n ${MINIO_BUCKET_NAMESPACE}| base64 --decode)
-export AWS_SECRET_ACCESS_KEY=$(kubectl get secret minio -o jsonpath="{.data.secretkey}" -n ${MINIO_BUCKET_NAMESPACE}| base64 --decode)
+export AWS_ACCESS_KEY_ID=$(kubectl get secret --namespace ${MINIO_BUCKET_NAMESPACE} minio -o jsonpath="{.data.root-user}" | base64 -d)
+export AWS_SECRET_ACCESS_KEY=$(kubectl get secret --namespace ${MINIO_BUCKET_NAMESPACE} minio -o jsonpath="{.data.root-password}" | base64 -d)
 
 # Generate values.yaml
 cat > $1 <<- EOF
